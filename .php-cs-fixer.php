c2867ca52ec478c8aca8ba3d06f41c9f157ee5eb
abdelrahman gado <gadoua50@gmail.com>
2026-07-03 02:02:41 +0300

    Add configuration files for PHPStan, PHPUnit, and Rector
    - Introduced `phpstan.neon.dist` for PHPStan configuration with level 10 and paths to `src` and `tests`.
    - Created `phpunit.dist.xml` and `phpunit.xml` for PHPUnit setup, defining test suite and source directories with strict settings for coverage and deprecations.
    - Added `rector.php` for Rector configuration, specifying paths, PHP sets, and various code quality rules.

---
+++ .php-cs-fixer.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+use PhpCsFixer\Config;
+use PhpCsFixer\Finder;
+
+return (new Config())
+    ->setParallelConfig(PhpCsFixer\Runner\Parallel\ParallelConfigFactory::detect())
+    ->setRiskyAllowed(true)
+    ->setRules([
+        '@auto' => true,
+        '@auto:risky' => true,
+        '@PSR12' => true,
+        '@PSR12:risky' => true,
+        'array_push' => true,
+        'backtick_to_shell_exec' => true,
+        'declare_strict_types' => true,
+        'final_class' => true,
+        'strict_comparison' => true,
+        'modernize_strpos' => true,
+        'blank_line_after_opening_tag' => false,
+    ])
+    // 💡 by default, Fixer looks for `*.php` files excluding `./vendor/` - here, you can groom this config
+    ->setFinder(
+        (new Finder())
+            // 💡 root folder to check
+            ->in(__DIR__)
+            // 💡 additional files, eg bin entry file
+            // ->append([__DIR__.'/bin-entry-file'])
+            // 💡 folders to exclude, if any
+            ->exclude(['vendor', '.*'])
+        // 💡 path patterns to exclude, if any
+        // ->notPath([/* ... */])
+        // 💡 extra configs
+        // ->ignoreDotFiles(false) // true by default in v3, false in v4 or future mode
+        // ->ignoreVCS(true) // true by default
+    );
--- Dockerfile
+++ Dockerfile
@@ -1,17 +1,17 @@
 FROM php:8.3-cli-alpine
 
-WORKDIR /app
+WORKDIR /home/app
 
 RUN apk add --no-cache \
     git \
     bash \
     unzip \
     && rm -rf /var/cache/apk/*
-    
+
 COPY --from=composer:latest /usr/bin/composer /usr/bin/composer
 
 COPY . .
 
 RUN composer install --no-dev --no-interaction --optimize-autoloader
 
-CMD ["php", "./src/main.php"]
+CMD ["tail", "-f", "/dev/null"]
